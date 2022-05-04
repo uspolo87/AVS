4cfa36651bb5dc1b04422758c1e5ce92a47d9807
Naveen kumar pulikonda <pulikonda@Naveens-MacBook-Air.local>
2022-05-04 15:58:05 +0530

    first push of code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { CalnderComponent } from './calnder/calnder.component';
+import { ClassesComponent } from './classes/classes.component';
+import { RulesComponent } from './rules/rules.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    HomeComponent,
+    CalnderComponent,
+    ClassesComponent,
+    RulesComponent
   ],
   imports: [
     BrowserModule,
