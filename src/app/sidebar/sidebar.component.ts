f3d02e5e2f48cdd7f1e2c80a108be7adb58add78
Naveen kumar pulikonda <pulikonda@Naveens-MacBook-Air.local>
2022-05-16 23:03:41 +0530

    project done

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  activeElement: string = 'dashboard';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  setActive(ele: string) {
+    this.activeElement = ele;
   }
-
 }
